71fa82040f5aa5b954371e9fb7775164e03a4b38
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-11-09 14:05:09 -0500

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -18,11 +18,11 @@ public class ExampleJavaMod extends Mod{
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
+                BaseDialog dialog = new BaseDialog("Communistry");
+                dialog.cont.add("Godspeed").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
                 dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.cont.button("Affirmitive", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
         });
